f88db88eeff258f29251ef2c0ba34c53bf188d63
Luis Mateo Coronado Laine <luis.coronado@epn.edu.ec>
2025-01-10 14:25:58 -0500

    tarea9

---
+++ Tareas/[Tarea 09]/src/__init__.py
@@ -0,0 +1,11 @@
+from .linear_sist_methods import (
+    eliminacion_gaussiana,
+    eliminacion_gaussiana2,
+    eliminacion_gaussiana3,
+    descomposicion_LU,
+    resolver_LU,
+    matriz_aumentada,
+    separar_m_aumentada,
+    eliminacion_gauss_jordan,
+    
+)
